a830ed37cd4ff5bde122aeecf9f9862b2bb15d4a
Stephanie  Wang <sxwang@lmdv-swang.int.godaddy.com>
2014-06-28 01:33:57 -0700

    init script

---
+++ init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+_done() {
+    echo "...done :)"
+}
+
+echo "Installing Python dependencies"
+sudo pip install flake8
+_done
+
+echo "Initializing git submodules"
+git submodule init
+git submodule update
+_done
+
+echo "Copying dotfiles"
+cp -R vim ~/.vim
+cp vim/vimrc ~/.vimrc
+_done
